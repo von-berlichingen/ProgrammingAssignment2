f55d2fabfa89cc14e14ebe1749a67f71467d68c4
von-berlichingen <anis.b.amara@gmail.com>
2015-05-24 19:17:54 +0100

    Fixing the 2nd function description

--- cachematrix.R
+++ cachematrix.R
@@ -28,8 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## The second function, cacheSolve calculates the inverse of the special "matrix" created with the above function.
 ## However, it first checks to see if the inverse has already been calculated.
 ## If so, it gets the inverse from the cache and skips the computation.
-## Else, it calculates the inverse of the data and sets the value of the inverse in the cache via the
-setsolve function.
+## Else, it calculates the inverse of the data and sets the value of the inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
         inv <- x$getsolve()
